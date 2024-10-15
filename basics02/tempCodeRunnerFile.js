7ddb97a8106ea3f03da89bc015d7d17ecf720ccb
Amardeep <vermaamardeep86@gmail.com>
2024-10-15 23:33:29 +0530

    Objects

--- basics02/tempCodeRunnerFile.js
+++ basics02/tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-console.log(JsUser.location);
-// console.log(JsUser["location"]);
-// console.log(JsUser["full name "]);
-
-// JsUser.location = "Mohali"
-// console.log(JsUser.location);
+person.lastName
